03a9b65f2e9cf53a086bc9aafbd74cba7052900c
akharlamov <akharlamov@google.com>
2018-08-07 11:50:19 -0700

    Fix compiler warning on Linux builds.
    Change-Id: I4b4838a99e9893af186f31af395a0559dbf25e38

--- renderdoc/serialise/codecs/vk_cpp_codec_writer_template_files.cpp
+++ renderdoc/serialise/codecs/vk_cpp_codec_writer_template_files.cpp
@@ -21,7 +21,6 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
 ******************************************************************************/
-#pragma once
 #include "vk_cpp_codec_writer.h"
 
 namespace vk_cpp_codec
