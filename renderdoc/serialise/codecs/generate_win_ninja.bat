835f8ab873d20c0b1dc677f8bf4282af9187f3e3
akharlamov <akharlamov@google.com>
2018-08-06 20:33:03 -0700

    Fix how this script calls vcvarsall.bat.
    Change-Id: I97df8a57221789dbd8b9e65ba3d9c4f4bb369fa6

--- renderdoc/serialise/codecs/generate_win_ninja.bat
+++ renderdoc/serialise/codecs/generate_win_ninja.bat
@@ -5,7 +5,7 @@ rd /s /q build_x64
 mkdir build_x64\Debug
 mkdir build_x64\Release
 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 "C:\Program Files\CMake\bin\cmake.exe" -Wno-dev -G Ninja --build "" -H. -BWinNinja -DCMAKE_BUILD_TYPE=Release
 
 cd WinNinja
