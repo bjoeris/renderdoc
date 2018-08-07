5ff0ab10ec42f83fb213d2d7f9b9279871a29ef5
akharlamov <akharlamov@google.com>
2018-08-06 18:53:12 -0700

    Rename copy_code_gen to better reflect it's current usage.
    Use this script to quickly clean up the sample_cpp_trace folder.
    
    Change-Id: I424a468376039620629859163672ff99e727c37a

---
+++ renderdoc/serialise/codecs/clean_codegen_sample_cpp_trace.bat
@@ -0,0 +1,6 @@
+del sample_cpp_trace\gen_*
+del sample_cpp_trace\buffer_*
+del sample_cpp_trace\shader_*
+del sample_cpp_trace\pipeline_cache_*
+del sample_cpp_trace\*shim_vulkan*
+del sample_cpp_trace\sample_cpp_trace*
